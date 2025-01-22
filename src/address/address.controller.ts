e66dea0963663183a7f4c6946c3687a1fd24344d
Brian <sbriansantos2002@gmail.com>
2025-01-22 13:04:50 -0300

    feat: add address module with controller, service, and DTO for address creation

---
+++ src/address/address.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { AddressService } from './address.service';
+import { CreateAddressDto } from './dtos/createAddress.dto';
+import { AddressEntity } from './entities/address.entity';
+
+@Controller('address')
+export class AddressController {
+  constructor(private readonly addressService: AddressService) {}
+
+  @Post(':userId')
+  async createAddress(
+    @Body() createAddressDto: CreateAddressDto,
+    @Param('userId') userId: number,
+  ): Promise<AddressEntity> {
+    return this.addressService.createAddress(createAddressDto, userId);
+  }
+}
