004743e404e23c9a828a9c23d061f1ef72b1a2dc
Brian <sbriansantos2002@gmail.com>
2025-01-24 19:43:00 -0300

    feat: refactor address creation to use custom user ID decorator and add base64 authorization utility

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,19 +1,20 @@
-import { Body, Controller, Param, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AddressService } from './address.service';
 import { CreateAddressDto } from './dtos/createAddress.dto';
 import { AddressEntity } from './entities/address.entity';
 import { Roles } from 'src/decorators/roles.decorator';
 import { UserType } from 'src/users/enums/userType.enum';
+import { UserId } from 'src/decorators/userId.decorator';
 
 @Roles(UserType.User)
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
-  @Post(':userId')
+  @Post()
   async createAddress(
     @Body(new ValidationPipe()) createAddressDto: CreateAddressDto,
-    @Param('userId') userId: number,
+    @UserId() userId: number,
   ): Promise<AddressEntity> {
     return this.addressService.createAddress(createAddressDto, userId);
   }
---
+++ src/decorators/userId.decorator.ts
@@ -0,0 +1,10 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { authorizantionToLoginPayload } from 'src/utils/base64Converter';
+
+export const UserId = createParamDecorator((_, ctx: ExecutionContext) => {
+  const { authorization } = ctx.switchToHttp().getRequest().headers;
+
+  const loginPayload = authorizantionToLoginPayload(authorization);
+
+  return loginPayload?.id;
+});
